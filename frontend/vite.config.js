a8f1e13fae819ebc8b3bc023b944b1a92171ca46
thenetaji <y.himesh026+github@gmail.com>
2024-12-08 00:29:31 +0530

    v1.0.2 Added other html files as it was not included by vite as well as changed input box id to seperate from other sites input box for auto completion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,14 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        about: "./src/pages/about.html",
+        disclaimer: "./src/pages/disclaimer.html",
+        contact: "./src/pages/contact.html",
+      },
+    },
   },
   server: {
     open: true,
